a2910e4199ed95ca2d4b1479530a0ae75c7d3d6d
Ahmed Folly <ahmedfolly124@gmail.com>
2025-02-15 23:40:57 +0200

    delete test files

--- app/src/main/java/com/example/ramadanapp/common/data/repos/local/AppDatabase.kt
+++ app/src/main/java/com/example/ramadanapp/common/data/repos/local/AppDatabase.kt
@@ -3,6 +3,4 @@ package com.example.ramadanapp.common.data.repos.local
 import androidx.room.RoomDatabase
 
 
-abstract class AppDatabase: RoomDatabase() {
-
-}
+abstract class AppDatabase: RoomDatabase()
