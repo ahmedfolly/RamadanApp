2f7d19697d32d879e23a15e5df06f91751ffbaf6
Ahmed Folly <ahmedfolly124@gmail.com>
2025-02-24 11:27:53 +0200

    edit response

--- app/src/main/java/com/example/ramadanapp/features/media/data/model/dtos/PlaylistDto.kt
+++ app/src/main/java/com/example/ramadanapp/features/media/data/model/dtos/PlaylistDto.kt
@@ -4,6 +4,6 @@ import com.example.ramadanapp.features.media.domain.model.Video
 import com.google.gson.annotations.SerializedName
 
 data class PlaylistDto(
-    @SerializedName("data")
+    @SerializedName("items")
     val videos: List<VideoDto>?=null
 )
